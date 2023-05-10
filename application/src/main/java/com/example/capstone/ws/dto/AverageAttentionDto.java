d4fa5474488f0eaaf6bbf6de442b16200d635adc
mariasalmasian <maria.salmasian@qless.com>
2023-05-10 12:24:22 +0400

    converted attentionDto to record

--- application/src/main/java/com/example/capstone/ws/dto/AverageAttentionDto.java
+++ application/src/main/java/com/example/capstone/ws/dto/AverageAttentionDto.java
@@ -1,12 +1,6 @@
 package com.example.capstone.ws.dto;
 
 import lombok.Builder;
-import lombok.Data;
 
-@Data
 @Builder
-public class AverageAttentionDto {
-
-    private String identifier;
-    private Double percentage;
-}
+public record AverageAttentionDto(String identifier, Double percentage) { }
